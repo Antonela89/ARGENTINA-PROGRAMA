d45be01d1b0f633a5f29ba95c905c19b98f7d477
Antonela Borgogno <antonelaborgogno@gmail.com>
2022-04-24 16:46:29 -0300

    ARENTINA PROGRAMA 1

---
+++ EJERCICIOS/TypeScript/example01.ts
@@ -0,0 +1,5 @@
+let a: number; //* Inferencia explícita
+let b: string; //* Inferencia explícita
+let c=101; //* Inferencia implícita
+
+let recursos: string [] =['memoria','disco','procesador'] //* Es aconsejable espicificar el tipo de datos de una variable para no incurrir a errores
---
+++ EJERCICIOS/TypeScript/output/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let a; //* Inferencia explícita
+let b; //* Inferencia explícita
+let c = 101; //* Inferencia implícita
+let recursos = ['memoria', 'disco', 'procesador']; //* Es aconsejable espicificar el tipo de datos de una variable para no incurrir a errores
+let message = 'Hello World';
+console.log(message);
